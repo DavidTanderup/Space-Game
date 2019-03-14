54dbe59fd2aa462d8bcb8df2fc7592221c69d2f8
Parker Eastman <ParkerEastman@outlook.com>
2019-03-14 12:10:33 -0400

    Base Character class created, properties will be used to track different aspects of player

---
+++ Characters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ships;
+
+namespace SpaceCadets
+{
+    class Characters
+    {
+        public string Name;
+        public int money;
+        public double Age;
+        string location;
+        SpaceShip mySpaceShip = new SpaceShip();
+        
+        //skills
+        //distance traveled
+
+
+        public Characters(int money, double Age, string location)
+        {           
+            this.money = money;
+            this.Age = Age;
+            this.location = location;
+           `
+        }
+
+    }
+}
