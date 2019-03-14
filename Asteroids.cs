2994461f7d1030f2e059a805858d86cafe3216f1
Parker Eastman <ParkerEastman@outlook.com>
2019-03-14 12:09:17 -0400

    Asteroid Class that will constitute elements of mini-game

---
+++ Asteroids.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp4
+{
+
+    class Coordinate
+    {
+
+        public int X { get; set; } //Left
+        public int Y { get; set; } //Top
+    }
+
+    class Asteroids
+    {
+        Random ran = new Random();
+
+        //string name = "Asteroid";
+        //int speed = 0;
+        //string shape = "*";
+        //Coordinate ast = new Coordinate();
+
+
+
+
+        public Coordinate spawnAsteroid(int width, int height)  //necessary?
+        {
+            Coordinate ast = new Coordinate();
+            ast.Y = ran.Next(0, height);
+            ast.X = width;
+            Console.SetCursorPosition(ast.X, ast.Y);
+            Console.Write("O");
+            return ast;
+        }
+
+        //
+
+
+        public List<Coordinate> AsteroidMover(List<Coordinate> AsteroidList)
+        {
+            
+            Coordinate ast = new Coordinate();
+            for (int i = 0; i < AsteroidList.Count; i++)
+            {
+                ast = AsteroidList[i];
+                ast.X -= 1;
+                if (ast.X == 0)
+                {
+                    AsteroidList.RemoveAt(i);
+                    Console.SetCursorPosition(ast.X + 1, ast.Y);
+                    Console.Write(" ");
+                    continue;
+                }
+                AsteroidList.RemoveAt(i);
+                AsteroidList.Insert(i, ast);
+                Console.SetCursorPosition(ast.X + 1, ast.Y);
+                Console.Write(" ");
+                Console.SetCursorPosition(ast.X, ast.Y);
+                Console.Write("O");
+            }
+            return AsteroidList;
+        }
+
+    }
+
+
+
+    
+    
+    
+    //Spawn Asteroid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
