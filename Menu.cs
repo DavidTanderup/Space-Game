824e25def787bc5212e829f83648a009f60d3283
Parker Eastman <ParkerEastman@outlook.com>
2019-03-14 21:31:05 -0400

    Corrected errors preventing compilation.

--- Menu.cs
+++ Menu.cs
@@ -9,13 +9,51 @@ namespace SpaceCadets
     class Menus
     {
 
-        //Console.ReadKey
-        //switch(action) //Travel, Trade, Fuel, Mine
-        //    {
+        ConsoleKeyInfo action;
+        public void MainMenu()
+        {
+            while (true)
+            {
+                //switch (action.Key) //Travel, Trade, Fuel, Mine
+                //{
+                //    case ConsoleKey.UpArrow:
+                //        Console.WriteLine("up");
+                //        System.Threading.Thread.Sleep(1000);
+                //        break;
+
+                //    case ConsoleKey.DownArrow:
+                //        Console.WriteLine("down");
+                //        break;
+
+                //    case ConsoleKey.RightArrow:
+                //        Console.WriteLine("right");
+                //        break;
+
+                //}
 
 
-        //    }
 
-        
+                ConsoleKeyInfo keyInfo;
+                if (Console.KeyAvailable)
+                {
+                    keyInfo = Console.ReadKey(true);
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            Console.WriteLine("up");                        //KeyPress = true;
+                            break;
+
+                        case ConsoleKey.RightArrow:
+                            Console.WriteLine("something");                            //KeyPress = true;
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            Console.WriteLine("something");                            //KeyPress = true;
+                            break;
+                    }
+                }
+            }
+
+        }
     }
 }
