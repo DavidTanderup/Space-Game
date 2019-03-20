5c5868798d9f00600980ba87024aae38713e4c30
Parker Eastman <ParkerEastman@outlook.com>
2019-03-20 16:11:22 -0400

    Integrated money/cost into moonmarket/part definitions

--- SpaceShip.cs
+++ SpaceShip.cs
@@ -52,18 +52,18 @@ namespace Ships
 
     
         //Engines
-        public Engines Engine1 = new Engines("WX100", ">=", 1, 100);
-        public Engines Engine2 = new Engines("WX200", ">>==", 2, 300);
-        public Engines Engine3 = new Engines("WX300", ">>>===", 2.5, 500);
+        public Engines Engine1 = new Engines("WX100", ">=", 1, 100, 0);
+        public Engines Engine2 = new Engines("WX200", ">>==", 2, 300, 2000);
+        public Engines Engine3 = new Engines("WX300", ">>>===", 2.5, 500, 5000);
 
         //Fuel
-        public Fuel Fuel1 = new Fuel("-MiniGulp", "0",500 ,2000, 500);
-        public Fuel Fuel2 = new Fuel("-MidGulp", "00",1000 ,3500, 1000);
-        public Fuel Fuel3 = new Fuel("-BigGulp", "000", 2000 ,5000, 2000);
+        public Fuel Fuel1 = new Fuel("-MiniGulp", "0",500 ,2000, 500, 0);
+        public Fuel Fuel2 = new Fuel("-MidGulp", "00",1000 ,3500, 1000, 2000);
+        public Fuel Fuel3 = new Fuel("-BigGulp", "000", 2000 ,5000, 2000, 5000);
         //Cargo
-        public Cargo Cargo1 = new Cargo("-spacebay", "||", 1000, 0);
-        public Cargo Cargo2 = new Cargo("-spaceBAY", "|||", 2000, 0);
-        public Cargo Cargo3 = new Cargo("-spaceBAE", "||||", 3000, 0);
+        public Cargo Cargo1 = new Cargo("-spacebay", "||", 1000, 0, 0);
+        public Cargo Cargo2 = new Cargo("-spaceBAY", "|||", 2000, 0, 2000);
+        public Cargo Cargo3 = new Cargo("-spaceBAE", "||||", 3000, 0, 5000);
 
         public string nose = "=>";
         //nav
@@ -79,13 +79,15 @@ namespace Ships
         public string rep = ">";
         public double speed = 0;
         public double weight = 0;
+        public double cost = 0;
 
-        public Engines(string name, string rep, double speed, int weight)
+        public Engines(string name, string rep, double speed, double weight, double cost)
         {
         this.name = name;
         this.rep = rep;
         this.speed = speed;
         this.weight = weight;
+        this.cost = cost;
 
         }
     }
@@ -97,15 +99,17 @@ namespace Ships
         public double capacity = 0;
        // public int fuel = 0;
         public double weight = 0;
-       
+        public double cost = 0;
 
-        public Fuel(string name, string rep, int fuel, int capacity, int weight)
+
+        public Fuel(string name, string rep, double fuel, double capacity, double weight, double cost)
         {
             this.name = name;
             this.rep = rep;
             //this.fuel = fuel;
             this.capacity = capacity;
             this.weight = weight; //This will be how much fuel is on board!!!
+            this.cost = cost;
         }
     }
 
@@ -115,13 +119,15 @@ namespace Ships
         public string rep = "";
         public double capacity = 0;
         public double weight = 0;
+        public double cost = 0;
 
-        public Cargo(string name, string rep, int capacity, int weight)
+        public Cargo(string name, string rep, double capacity, double weight, double cost)
         {
             this.name = name;
             this.rep = rep;
             this.capacity = capacity;
             this.weight = weight;
+            this.cost = cost;
         }
 
 
